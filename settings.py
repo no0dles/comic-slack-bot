3626a6ad3ba8ce4ac26525402e227616e1e58f36
Pascal <pascal.bertschi@hsr.ch>
2016-04-19 09:21:07 +0200

    fix setting issues

--- settings.py
+++ settings.py
@@ -1,18 +1,8 @@
 import os
-from urllib.parse import urlparse
 
-if 'HEROKU' in os.environ:
-    DEBUG = False
-    DATABASE_URL = urlparse(os.environ['DATABASE_URL'])
-    DATABASE = {
-        'name': DATABASE_URL.path[1:],
-        'user': DATABASE_URL.username,
-        'password': DATABASE_URL.password,
-        'host': DATABASE_URL.hostname,
-        'port': DATABASE_URL.port,
-    }
+if 'DATABASE_URL' in os.environ:
+    DATABASE_URL = os.environ['DATABASE_URL']
 else:
-    DEBUG = True
     DATABASE_URL = None
     DATABASE = {
         'name': 'comicbot',
@@ -23,4 +13,10 @@ else:
         'threadlocals': True
     }
 
-HOOK_URL = os.environ['HOOK_URL']
+if 'HEROKU' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
+
+if 'HOOK_URL' in os.environ:
+    HOOK_URL = os.environ['HOOK_URL']
